69a41cc8fb460f952e2471de341a707594f22829
Jacob Torres <jacobangelo_torres@yahoo.com>
2020-05-30 12:12:43 -0700

    Test mysql db connection

--- src/com/jtorres/testdb/testdbservlet.java
+++ src/com/jtorres/testdb/testdbservlet.java
@@ -1,11 +1,15 @@
 package com.jtorres.testdb;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.sql.*;
 
 /**
  * Servlet implementation class testdbservlet
@@ -18,8 +22,36 @@ public class testdbservlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// setup connection variagbles
+		
+		String user = "jtorres";
+		String pass = "pw12345";
+		
+		String jdbcurl = "jdbc:mysql://localhost:3306/springexercise?useSSL=false&serverTimezone=UTC";
+		String driver = "com.mysql.cj.jdbc.Driver";
+		// get connection to db
+		
+		try {
+			
+			PrintWriter out = response.getWriter();
+			
+			out.println("Connecting to DB: " + jdbcurl);
+			
+			Class.forName(driver);
+			
+			Connection myconn = DriverManager.getConnection(jdbcurl, user, pass);
+			
+			out.println("Successfully connected!");
+			
+			myconn.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ServletException(e);
+		}
+		
+		
+		
 	}
 
 }
