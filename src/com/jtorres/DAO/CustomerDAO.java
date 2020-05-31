3a30d8cdd243e420bc188fd7bb562052541d8f88
Jacob Torres <jacobangelo_torres@yahoo.com>
2020-05-30 21:11:56 -0700

    Add DAO

---
+++ src/com/jtorres/DAO/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.jtorres.DAO;
+
+import java.util.List;
+
+import com.jtorres.springexercise.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+
+}
---
+++ src/com/jtorres/DAO/CustomerDAOImpl.java
@@ -0,0 +1,35 @@
+package com.jtorres.DAO;
+
+import java.util.List;
+
+import org.hibernate.query.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.jtorres.springexercise.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class);
+		
+		List<Customer> customers = theQuery.getResultList();
+		
+		return customers;
+	
+	}
+
+}
